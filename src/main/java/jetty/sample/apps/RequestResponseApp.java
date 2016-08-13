22b823de5dd6129ef60363b03eb6c9cfbc494d7c
batako-san <hkawabata_work@yahoo.co.jp>
2016-08-13 20:11:04 +0900

    add form for get/post method and impliment doPost in RequestResponseApp.java

--- src/main/java/jetty/sample/apps/RequestResponseApp.java
+++ src/main/java/jetty/sample/apps/RequestResponseApp.java
@@ -26,4 +26,21 @@ public class RequestResponseApp extends HttpServlet {
             out.println(String.format("{'error': '%s: %s'}", cls, msg));
         }
     }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException{
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            int id = Integer.parseInt(request.getParameter("id"));
+            String pass = request.getParameter("pass");
+            String resString = String.format("your ID is %d, your password is %s", id, pass);
+            out.println("<html><body>" + resString + "</body></html>");
+        } catch(Exception e) {
+            //e.printStackTrace();
+            String cls = e.getClass().getName();
+            String msg = e.getLocalizedMessage();
+            out.println(String.format("<html><body>internal server error:<br/>%s<br/>%s</body></html>", cls, msg));
+        }
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -23,5 +23,16 @@ list.add("c");
         <%for (int i = 0; i < 3; i++) { %>
         <p>繰り返し文<%= list.get(i) %></p>
         <% } %>
+
+        <form action="/hello/world" method="get">
+            Hello World のページへ：
+            <input type="submit" value="ページ遷移">
+        </form>
+        <form action="/request/response" method="post">
+            Request/Response のページへ：<br>
+            ID: <input type="text" name="id">
+            pass:<input type="text" name="pass">
+            <input type="submit" value="ページ遷移">
+        </form>
     </body>
 </html>
